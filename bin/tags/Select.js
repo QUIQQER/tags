c9236bb09ec9246b801ff737f0d0a2889a377cc5
Patrick Müller <p.mueller@pcsg.de>
2026-08-05 09:21:14 +0200

    fix: create tags from tag groups

--- bin/tags/Select.js
+++ bin/tags/Select.js
@@ -305,8 +305,12 @@ define('package/quiqqer/tags/bin/tags/Select', [
                     projectName: this.$Project.getName(),
                     projectLang: this.$Project.getLang(),
                     tag        : tag,
+                    tagParams  : JSON.encode({}),
                     showError  : false,
-                    onError    : reject
+                    onError    : () => {
+                        this.Loader.hide();
+                        reject();
+                    }
                 });
             });
         },
